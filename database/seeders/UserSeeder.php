ab38bff53d98785736e77fde3f1f2a44e05b320e
chinguyen98 <dacchi6761@gmail.com>
2020-11-11 15:32:05 +0700

    update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        $limit = 5;
+
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->email,
+                'password' => Hash::make($faker->password),
+            ]);
+        }
+    }
+}
