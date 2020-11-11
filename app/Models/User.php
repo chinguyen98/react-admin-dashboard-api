f096d301a0a394199f0f685187f1163a78618ff3
chinguyen98 <dacchi6761@gmail.com>
2020-11-11 21:47:11 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [
-
+            'name' => $this->name,
         ];
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+/* CORS */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
